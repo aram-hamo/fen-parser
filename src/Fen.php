7c04283088da8becff6c7079f27ccfe288b0f196
Aram Hamo <contact@aramhamo.me>
2023-11-13 20:00:07 +0100

    new file:   Fen.php deleted:    parser.php

---
+++ src/Fen.php
@@ -0,0 +1,50 @@
+<?php
+
+Class Fen{
+  public static function Generate(Array $board):String{
+    $empty_squares = 0;
+    $line_counter = 0;
+    $previous_square = null;
+
+    $fen = "";
+    foreach($board as $square){
+      if($line_counter == 8 ){
+        if($empty_squares !== 0){
+          $fen .= $empty_squares; 
+        }
+        $fen .= "/";
+        $line_counter = 0;
+        $empty_squares = 0;
+      }
+    
+      if($square == ''){
+        $empty_squares++;
+      }else{
+        if($previous_square == '' && $square !== ''){
+          if($empty_squares !== 0){
+            $fen .= $empty_squares;
+          }
+          $empty_squares = 0;
+        }
+        $fen .= $square;
+      }
+      $line_counter++;
+      $previous_square = $square;
+    
+    }
+    return $fen;
+  }
+}
+
+$board = [
+  'r','n','b','q','k','b','n','r',
+  'p','p','p','p','p','p','p','p',
+  '','','','','','','','',
+  '','','','','','','','',
+  '','','','','','','','',
+  '','','','','','','','',
+  'P','P','P','P','P','P','P','P',
+  'R','N','B','Q','K','B','N','R'
+];
+
+echo Fen::Generate($board);
