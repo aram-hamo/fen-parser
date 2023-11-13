96a7af62e671034bfb0e9983af6cf73d57ca2bb5
Aram Hamo <contact@aramhamo.me>
2023-11-13 19:40:47 +0100

    modified:   src/parser.php

--- src/parser.php
+++ src/parser.php
@@ -5,6 +5,39 @@ $board = [
   'p','p','p','p','p','p','p','p',
   '','','','','','','','',
   '','','','','','','','',
+  '','','','','','','','',
+  '','','','','','','','',
   'P','P','P','P','P','P','P','P',
-  'R','N','B','K','Q','B','N','R'
+  'R','N','B','Q','K','B','N','R'
 ];
+
+$empty_squares = 0;
+$line_counter = 0;
+$previous_square = null;
+
+foreach($board as $square){
+  if($line_counter == 8 ){
+    if($empty_squares !== 0){
+      echo $empty_squares; }
+    echo '/';
+    $line_counter = 0;
+    $empty_squares = 0;
+  }
+
+  if($square == ''){
+    $empty_squares++;
+  }else{
+    if($previous_square == '' && $square !== ''){
+      if($empty_squares !== 0){
+        echo $empty_squares;
+      }
+      $empty_squares = 0;
+    }
+    echo $square;
+  }
+  $line_counter++;
+  $previous_square = $square;
+
+}
+
+echo "\n";
